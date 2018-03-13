cc57ecedf094dacb4c8f6f300473e1d7e031ee10
Sergey Kacheev <skacheev@yandex-team.ru>
2018-03-13 17:05:13 +0700

    parsing init

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,28 +1,79 @@
-#[derive(Debug, Hash)]
+use std::io::{self, BufRead, BufReader};
+use std::fs::File;
+use std::collections::HashSet;
+
+#[derive(Debug, Hash, PartialEq)]
 pub struct Nonterminal {
-    pub symbol: String,
+    pub symbol: char,
+}
+impl Eq for Nonterminal {}
+
+impl Nonterminal {
+    pub fn new(from: char) -> Nonterminal {
+        Nonterminal {
+            symbol: from,
+        }
+    }
 }
 
-#[derive(Debug, Hash)]
+#[derive(Debug, Hash, PartialEq)]
 pub struct Terminal {
-    pub symbol: String,
+    pub symbol: char,
 }
 
-#[derive(Debug, Hash)]
+impl Terminal {
+    pub fn new(from: char) -> Terminal {
+        Terminal {
+            symbol: from,
+        }
+    }
+}
+
+
+#[derive(Debug, Hash, PartialEq)]
 pub enum Symbol {
     N(Nonterminal),
     T(Terminal),
 }
 
-#[derive(Debug, Hash)]
+#[derive(Debug, Hash, PartialEq)]
 pub struct Production {
     pub left: Nonterminal,
     pub right: Vec<Symbol>,
 }
 
-#[derive(Debug, Hash)]
+impl Eq for Production {}
+
 pub struct CFG {
     pub start: Nonterminal,
-    pub productions: Vec<Production>,
-    pub categories: Vec<Nonterminal>,
+    pub productions: HashSet<Production>,
+    pub categories: HashSet<Nonterminal>,
+}
+
+impl CFG {
+    pub fn parse(inputPath: &str) -> io::Result<CFG> {
+        let mut cfg: CFG;
+        let mut file = BufReader::new(
+            File::open(inputPath).expect(
+                format!("opening file {}", inputPath).as_ref()
+            )
+        );
+        let mut it = file.lines();
+        match it.next() {
+            Some(s) => {
+                println!("{:?}", s);
+            }
+            None => {
+                return Err(io::Error::new(io::ErrorKind::Other, "Don't see any rule",));
+            }
+        }
+        for line in it {
+            println!("{:?}", line);
+        }
+        Ok(CFG{
+            start: Nonterminal::new('a'),
+            productions: HashSet::new(),
+            categories: HashSet::new(),
+        })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-#[macro_use]
 extern crate clap;
 
 mod args;
 mod cfg;
 
 fn main() {
-    let app = args::build_app("bmper");
+    let app = args::build_app("plt");
 
-    if let Some(matches) = app.subcommand_matches("generate") {
+    if let Some(matches) = app.subcommand_matches("gen") {
+        let grammar = matches.value_of("CFG").unwrap();
+        let cfg = cfg::CFG::parse(grammar).unwrap();
         println!("Yep!");
     }
 }
